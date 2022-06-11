e2c77046c7140c6b4033c85753e87c68f43d5eff
Martin Heberling <martin.heberling.dalies@gmail.com>
2022-06-11 14:39:52 -0700

    Read lines from a file.

--- todoist.sh
+++ todoist.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hello world!"
+# Gets from where the file is being called from.
+STR=$(pwd)
+echo $STR
+
+FILE="test.py"
+
+# Read all lines from a file. First part is to simply read all lines, and
+# second part is to also read trailing partial lines.
+while IFS= read -r line || [[ -n "$line" ]];
+do
+  echo "$line"
+done < $FILE
