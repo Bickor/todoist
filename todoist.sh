57cf52192e1275078739e2d68fcd752c856e8967
Martin Heberling <martin.heberling.dalies@gmail.com>
2022-06-18 22:49:31 -0700

    Make code into a function.

--- todoist.sh
+++ todoist.sh
@@ -4,24 +4,28 @@
 CURDIR=$(pwd)
 STR="TODO"
 
-# Get every file in this folder
-for file in /Users/martinheberling/Desktop/Projects/todoist/*
-do
-  # Check if the file is actually a directory
-  if [[ -d $file ]];
-  then
-    echo "Directory!"
-  else
-    # Read all lines from a file. First part is to simply read all lines, and
-    # second part is to also read trailing partial lines.
-    while IFS= read -r line || [[ -n "$line" ]];
-    do
-      
-      # Print every line
-      if [[ $line == *"$STR"* ]];
-      then 
-        echo "$line"
-      fi
-    done < $file
-  fi
-done
+function todoist {
+  # Get every file in this folder
+  for file in /Users/martinheberling/Desktop/Projects/todoist/*
+  do
+    # Check if the file is actually a directory
+    if [[ -d $file ]];
+    then
+      echo "Directory!"
+    else
+      # Read all lines from a file. First part is to simply read all lines, and
+      # second part is to also read trailing partial lines.
+      while IFS= read -r line || [[ -n "$line" ]];
+      do
+        
+        # Print every line
+        if [[ $line == *"$STR"* ]];
+        then 
+          echo "$line"
+        fi
+      done < $file
+    fi
+  done
+}
+
+todoist
